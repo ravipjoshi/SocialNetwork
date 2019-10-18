e2224ff6f1692f4b2e5c2477a8297e122f8589fe
Ravi Joshi <ravipiyushjoshi@gmail.com>
2019-10-18 15:25:17 -0400

    created apiDocs with route information for front end Developers

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const mongoose = require ("mongoose");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser")
 const expressValidator = require("express-validator");
+const fs = require("fs")
 
 dotenv.config();
 
@@ -15,14 +16,33 @@ dotenv.config();
 mongoose.connect(process.env.MONGO_URI,
     {useNewUrlParser : true}
      ).then(()=> console.log("db connected"));
-//bring in routes
+
 
 mongoose.connection.on("error",err =>{
     console.log(`Db Connection Error`);
 })
+
+
+// Different routes to bring in
 const postRoutes = require('./routes/post');
 const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/user');
+
+//apiDocs
+
+app.get("/",(req,res)=>{
+
+    fs.readFile("docs/apiDocs.json",(err,data) =>{
+        if(err){
+            res.status(400).json({
+                error:err
+                
+            })
+        }
+        const docs = JSON.parse(data)
+        res.json(docs);
+    });
+});
 
 
 
--- routes/post.js
+++ routes/post.js
@@ -12,13 +12,13 @@ const {userById} = require("../controllers/user")
 const router = express.Router();
 
 
-router.get("/",getPosts);
+router.get("/posts",getPosts);
 router.post("/post/new/:userId",requireSignin,createPost,createPostValidator);
 router.get("/posts/by/:userId",requireSignin,postByUser)
 router.delete("/post/:postId",requireSignin,isPoster, deletePost)
 router.put("/post/:postId",requireSignin,isPoster, updatePost)
 
-
+//
 
 //any routes cpnntaining userid , app will first execute userbyID
 router.param("userId", userById);
