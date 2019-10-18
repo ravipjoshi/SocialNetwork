a5480a48294e2e76c4e486374b6d0628eeb2d9a5
Ravi Joshi <ravipiyushjoshi@gmail.com>
2019-10-18 14:34:04 -0400

    Updated post route with postbyid

--- routes/post.js
+++ routes/post.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {getPosts,createPost,postByUser} = require("../controllers/post")
+const {getPosts,createPost,postByUser,postById} = require("../controllers/post")
 const {createPostValidator} = require("../validator")
 const {requireSignin} = require("../controllers/auth")
 const {userById} = require("../controllers/user")
@@ -9,8 +9,13 @@ const router = express.Router();
 router.get("/",getPosts);
 router.post("/post/new/:userId",requireSignin,createPost,createPostValidator);
 router.get("/posts/by/:userId",requireSignin,postByUser)
+
 //any routes cpnntaining userid , app will first execute userbyID
 router.param("userId", userById);
+
+
+//any routes containing postid , app will first execute userbyID
+router.param("userId", postById);
 
 
 module.exports = router;
