cb79ac8b6b93662a2bd48857a5273c5696504fac
Ravi Joshi <ravipiyushjoshi@gmail.com>
2019-10-18 13:46:23 -0400

    added postbyid method in post controller

--- routes/post.js
+++ routes/post.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {getPosts,createPost} = require("../controllers/post")
+const {getPosts,createPost,postByUser} = require("../controllers/post")
 const {createPostValidator} = require("../validator")
 const {requireSignin} = require("../controllers/auth")
 const {userById} = require("../controllers/user")
@@ -7,9 +7,8 @@ const router = express.Router();
 
 
 router.get("/",getPosts);
-
 router.post("/post/new/:userId",requireSignin,createPost,createPostValidator);
-
+router.get("/posts/by/:userId",requireSignin,postByUser)
 //any routes cpnntaining userid , app will first execute userbyID
 router.param("userId", userById);
 
